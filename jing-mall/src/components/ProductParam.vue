b25cb49f7a6b2a902a019de895af864b68c98000
jingjing20 <1269928993@qq.com>
2020-03-19 23:39:38 +0800

    商品详情页交互实现

--- jing-mall/src/components/ProductParam.vue
+++ jing-mall/src/components/ProductParam.vue
@@ -48,6 +48,7 @@ export default {
   line-height: 70px;
   border-top: 1px solid $colorH;
   background-color: $colorG;
+  z-index: 10;
   &.is_fixed{
     position: fixed;
     top: 0;
--- jing-mall/src/views/index.vue
+++ jing-mall/src/views/index.vue
@@ -212,15 +212,15 @@ export default {
         this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)];
       })
     },
-    addCart(){
+    addCart(id){
       this.showModal = true
-      // this.axios.post('/carts',{
-      //   productId:id,
-      //   selected: true
-      // }).then((res)=>{
-      //   this.showModal = true;
-      //   this.$store.dispatch('saveCartCount',res.cartTotalQuantity);
-      // });
+      this.axios.post('/carts',{
+        productId:id,
+        selected: true
+      }).then((res)=>{
+        this.showModal = true;
+        this.$store.dispatch('saveCartCount',res.cartTotalQuantity);
+      });
     },
     goToCart(){
       this.$router.push('/cart');
